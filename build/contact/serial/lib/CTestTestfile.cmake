78588877bef1654a684fc7f8daca8137b238a1c8
shucheng05 <Yue.Ma23@student.xjtlu.edu.cn>
2024-04-23 17:44:47 +0800

    Initial commit

--- build/contact/serial/lib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shucheng/contact/serial/lib
-# Build directory: /home/shucheng/contact/build/contact/serial/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
